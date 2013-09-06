30b2f40fea2a7d7a6a1ff0684127cf10a4114a51
Franco Riberi <franco.riberi@tallertechnologies.com>
2013-09-06 16:09:38 -0300

    Used auto_ptr and EXIT_SUCCESS

--- src/namecheck.cpp
+++ src/namecheck.cpp
@@ -1,12 +1,11 @@
 #include "NamingConventionPlugin.h"
 #include "GCCPluginAPI.h"
 #include "GenericTraverser.h"
-
 extern "C"
 {
 #include "cp/cp-tree.h"
 }
-
+#include <memory>
 #include <iostream>
 
 int plugin_is_GPL_compatible;
@@ -19,44 +18,26 @@ static struct plugin_info naming_info =
     "Naming Convention Plugin"    // help
 };
 
-
 extern "C" void gate_callback(void*, void*)
 {
     // If there were errors during compilation,
     // let GCC handle the exit.
     //
-    if (errorcount || sorrycount)
-        return;
-
-    int r(0);
-
-    GenericTraverser traverser;
-    BasePlugin* plugin = new NamingConventionPlugin();
-    PluginAPI* api = new GCCPluginAPI();
-    plugin->initialize(api);
-
-    //
-    // Process AST. Issue diagnostics and set r
-    // to 1 in case of an error.
-    //
-    std::clog << "processing " << main_input_filename << std::endl;
-
-    traverser.traverse(global_namespace, plugin->getVisitor());
-    delete api;
-    delete plugin;
-
-    exit(r);
+    if (!(errorcount || sorrycount))
+    {        
+        GenericTraverser traverser;
+        const std::auto_ptr<BasePlugin> plugin(new NamingConventionPlugin());
+        const std::auto_ptr<PluginAPI> api(new GCCPluginAPI());
+        plugin->initialize(api.get());
+        std::clog << "processing " << main_input_filename << std::endl;
+        traverser.traverse(global_namespace, plugin->getVisitor());
+    }
+    exit(EXIT_SUCCESS); 
 }
 
 extern "C" int plugin_init(plugin_name_args* info, plugin_gcc_version* /* version */)
 {
-    int r(0);
-
     std::cerr << "starting " << info->base_name << std::endl;
-
-    //
-    // Parse options if any.
-    //
 
     // Disable assembly output.
     asm_file_name = HOST_BIT_BUCKET;
@@ -65,5 +46,5 @@ extern "C" int plugin_init(plugin_name_args* info, plugin_gcc_version* /* versio
     register_callback(info->base_name, PLUGIN_OVERRIDE_GATE, &gate_callback, 0);
     register_callback(info->base_name, PLUGIN_INFO, NULL, &naming_info);
 
-    return r;
+    return EXIT_SUCCESS;
 }
