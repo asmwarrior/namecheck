6869e5fcaf00712740000397092b3dce4313bd2e
Franco Riberi <franco.riberi@tallertechnologies.com>
2013-09-06 15:21:38 -0300

    Defined the rules as class members and declared inline functions

--- namecheck/NamingConventionChecker.h
+++ namecheck/NamingConventionChecker.h
@@ -4,13 +4,12 @@
 #include <string>
 #include <boost/regex.hpp>
 
-
 class NamingConventionChecker
 {
 public:
 
     NamingConventionChecker();
-    bool correct_typedef_name(const std::string &s, std::string &errmsg) const;
+    inline bool correct_typedef_name(const std::string &s, std::string &errmsg) const;
     bool correct_class_name(const std::string &s, std::string &errmsg) const;
     bool correct_struct_name(const std::string &s, std::string &errmsg) const;
     bool correct_global_const_name(const std::string &s, std::string &errmsg) const;
@@ -44,10 +43,31 @@ private:
 
     void ruleCamelCase(Rules& rules) const;
     void ruleCamelBack(Rules& rules) const;
+    void ruleGlobalCont(Rules& rules) const;
+    void ruleAttribute(Rules& rules) const;
+    void setRules();
     bool generic_checker(const std::string &s, const Rules& rules, std::string &errmsg) const;
 
     Regexs _regexs;
-    ErrorMsgs _errmsgs;
+    ErrorMsgs _errmsgs;    
+
+    //Rules for declaration
+    Rules _typedefRules;
+    Rules _classRules;
+    Rules _structRules;
+    Rules _globalConstRules;
+    Rules _enumTypeRules;
+    Rules _enumValueRules;
+    Rules _methodRules;
+    Rules _varRules;
+    Rules _attributeRules;
+    Rules _unionRules;
+    Rules _unionValueRules;
+    Rules _namespaceRules;
 };
+
+#define NAMING_CONVENTION__CHECKER_INLINE_H
+#include "NamingConventionCheckerInline.h"
+#undef NAMING_CONVENTION__CHECKER_INLINE_H
 
 #endif
