c76f3b6120a72e40ebd113f48d54777aeffa2f9d
Franco Riberi <franco.riberi@tallertechnologies.com>
2013-09-09 09:40:37 -0300

    Added regex to avoid underscore in method declaration and update test

--- src/NamingConventionChecker.cpp
+++ src/NamingConventionChecker.cpp
@@ -13,13 +13,13 @@
 NamingConventionChecker::NamingConventionChecker() : _regexs(NameRulesSize), _errmsgs(NameRulesSize)
 {
     _regexs[MatchStartWithUpper] = "^\\u.*?";
-    _regexs[MatchStartWithLower] = 
-    "^\\l.*?";
+    _regexs[MatchStartWithLower] = "^\\l.*?";
     _regexs[MatchStartUnderscore] = "^_\\l.*?";
     _regexs[MatchEndLower] = ".*?[^\\u_\\d]\\l?$";
     _regexs[MatchEndUpper] = ".*?\\u$";
     _regexs[MatchEndLowerRestricted] = ".*?\\l$";
-    _regexs[MatchUpperAndUnderscore] = "[A-Z_]*?";
+    _regexs[MatchUpperAndUnderscore] = "[\\u_]*?";
+    _regexs[MatchWithoutUndescore] = "^_?[\\l\\u\\d]*$";
 
     _errmsgs[MatchStartWithUpper] = "names should start with uppercase";
     _errmsgs[MatchStartWithLower] = "names should start with lowercase";
@@ -28,6 +28,7 @@ NamingConventionChecker::NamingConventionChecker() : _regexs(NameRulesSize), _er
     _errmsgs[MatchEndUpper] = "names should end with uppercase";
     _errmsgs[MatchEndLowerRestricted] = "names should end with lowercase";
     _errmsgs[MatchUpperAndUnderscore] = "names should be written with uppercase and underscore";
+    _errmsgs[MatchWithoutUndescore] = "names should not be written using underscore";
     setRules();
 }
 
@@ -61,12 +62,14 @@ bool NamingConventionChecker::genericChecker(const std::string& s, const Rules& 
 void NamingConventionChecker::ruleCamelCase(Rules& rules) const
 {
     rules.push_back(MatchStartWithUpper);
+    rules.push_back(MatchWithoutUndescore);
     rules.push_back(MatchEndLowerRestricted);
 }
 
 void NamingConventionChecker::ruleCamelBack(Rules& rules) const
 {
     rules.push_back(MatchStartWithLower);
+    rules.push_back(MatchWithoutUndescore);
     rules.push_back(MatchEndLower);
 }
 
@@ -80,5 +83,6 @@ void NamingConventionChecker::ruleGlobalCont(Rules& rules) const
 void NamingConventionChecker::ruleAttribute(Rules& rules) const
 {
     rules.push_back(MatchStartUnderscore);
+    rules.push_back(MatchWithoutUndescore);
     rules.push_back(MatchEndLower);
 }
