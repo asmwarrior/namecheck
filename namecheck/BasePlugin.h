6711b214a310dd3b3ba53db360c75378bfa54a12
Marcos Diaz <marcos.diaz@tallertechnologies.com>
2013-09-16 11:23:27 -0300

    Fixed bad inheritance in base plugin.

--- namecheck/BasePlugin.h
+++ namecheck/BasePlugin.h
@@ -50,23 +50,6 @@ protected:
     */
     PluginApi* _plugin;
 
-private:
-
-    virtual void visitStringLiteral(const GenericTree& /* decl */, const DeclarationName& /* name */) {}
-    virtual void visitVariableDeclaration(const GenericTree& /* decl */, const DeclarationName& /* name */, bool /* isConst */, const std::string& /*typeName*/) {}
-    virtual void visitEnumTypeDeclaration(const GenericTree& /* decl */, const DeclarationName& /* name */) {}
-    virtual void visitEnumValueDeclaration(const GenericTree& /* decl */, const DeclarationName& /* name */) {}
-    virtual void visitFunctionDeclaration(const GenericTree& /* decl */, const DeclarationName& /* name */) {}
-    virtual void visitParameterDeclaration(const GenericTree& /* decl */, const DeclarationName& /* name */, bool /* isConst */) {}
-    virtual void visitTypeDeclaration(const GenericTree& /* decl */, const DeclarationName& /* name */) {}
-    virtual void visitClassDeclaration(const GenericTree& /* decl */, const DeclarationName& /* name */) {}
-    virtual void visitStructDeclaration(const GenericTree& /* decl */, const DeclarationName& /* name */) {}
-    virtual void visitUnionDeclaration(const GenericTree& /* decl */, const DeclarationName& /* name */) {}
-    virtual void visitUnionValueDeclaration(const GenericTree& /* decl */, const DeclarationName& /* name */, bool /* isConst */, const std::string& /*typeName*/) {}
-    virtual void visitMethodDeclaration(const GenericTree& /* decl */, const AccessModifier /* access */, const DeclarationName& /* name */, bool /* isConst */) {}
-    virtual void visitAttributeDeclaration(const GenericTree& /* decl */, const AccessModifier /* access */, const DeclarationName& /* name */, bool /* isConst */, const std::string& /*typeName*/) {}
-    virtual void visitNamespaceDeclaration(const GenericTree& /* decl */, const DeclarationName& /* name */) {}
-    virtual void visitGlobalConstDeclaration(const GenericTree& /* decl */, const DeclarationName& /* name */) {}
 
 };
 
