b4dc01c7d9dd21c1d90869c3bccfdf2188694d96
Marcos Diaz <marcos.diaz@tallertechnologies.com>
2013-09-16 15:35:58 -0300

    Other doxygen in .h

--- namecheck/GenericTraverserInline.h
+++ namecheck/GenericTraverserInline.h
@@ -1,3 +1,10 @@
+/**
+* @brief this sets the visitor and traverse the given namespace.
+*
+*
+* @param ns this should be the global namespace to process.
+* @param visitor the visitor to set.
+*/
 inline void traverse(const GenericTree ns, GenericVisitor* visitor)
 {
     _visitor = visitor;
