56a2781794526acaf189d78482f1e197cf98e9b5
Franco Riberi <franco.riberi@tallertechnologies.com>
2013-09-18 15:00:58 -0300

    Fixed segmentationFault in RulesContainer class

--- tests/APIMock.cpp
+++ tests/APIMock.cpp
@@ -27,81 +27,81 @@ TEST(PluginAPITests, ClassNameTest)
     //correct names
     plugin.visitClassDeclaration(decl, "ClassName");
 
-    // plugin.visitClassDeclaration(decl, "class_name");
-    // plugin.visitClassDeclaration(decl, "_className");
-    // plugin.visitClassDeclaration(decl, "_ClassName");
-    // plugin.visitClassDeclaration(decl, "isclassname");
-    // plugin.visitClassDeclaration(decl, "toclassname");
-    // plugin.visitClassDeclaration(decl, "classNAME");
-    // plugin.visitClassDeclaration(decl, "CLASSNAME");
+    plugin.visitClassDeclaration(decl, "class_name");
+    plugin.visitClassDeclaration(decl, "_className");
+    plugin.visitClassDeclaration(decl, "_ClassName");
+    plugin.visitClassDeclaration(decl, "isclassname");
+    plugin.visitClassDeclaration(decl, "toclassname");
+    plugin.visitClassDeclaration(decl, "classNAME");
+    plugin.visitClassDeclaration(decl, "CLASSNAME");
 }
 
-// TEST(PluginAPITests, VariableAndMethodsNamesTest)
-// {
-//     NamingConventionPlugin plugin;
-//     APIMock api;
-//     GenericTree decl = NULL;
-//     plugin.initialize(&api);
-//     const bool isConst = false;
-//     const std::string typeName = "int";
-//     EXPECT_CALL(api, warning(_,_))
-//     .Times(13);
+TEST(PluginAPITests, VariableAndMethodsNamesTest)
+{
+    NamingConventionPlugin plugin;
+    APIMock api;
+    GenericTree decl = NULL;
+    plugin.initialize(&api);
+    const bool isConst = false;
+    const std::string typeName = "int";
+    EXPECT_CALL(api, warning(_,_))
+    .Times(13);
 
-//     //correct variable names
-//     plugin.visitVariableDeclaration(decl, "amount", isConst, typeName);
-//     plugin.visitVariableDeclaration(decl, "i", isConst, typeName);
-//     plugin.visitVariableDeclaration(decl, "amoutVisible", isConst, typeName);
-//     plugin.visitVariableDeclaration(decl, "isVisible", isConst, typeName);
+    //correct variable names
+    plugin.visitVariableDeclaration(decl, "amount", isConst, typeName);
+    plugin.visitVariableDeclaration(decl, "i", isConst, typeName);
+    plugin.visitVariableDeclaration(decl, "amoutVisible", isConst, typeName);
+    plugin.visitVariableDeclaration(decl, "isVisible", isConst, typeName);
 
-//     //incorrect variable names
-//     plugin.visitVariableDeclaration(decl, "Amout", isConst, typeName);
-//     plugin.visitVariableDeclaration(decl, "_amout", isConst, typeName);
-//     plugin.visitVariableDeclaration(decl, "AMOUT", isConst, typeName);
-//     plugin.visitVariableDeclaration(decl, "amountAvailablE", isConst, typeName);
-//     plugin.visitVariableDeclaration(decl, "22amout", isConst, typeName);
-//     plugin.visitVariableDeclaration(decl, "amoutVisible2", isConst, typeName);
-//     plugin.visitVariableDeclaration(decl, "amout_visible", isConst, typeName);
+    //incorrect variable names
+    plugin.visitVariableDeclaration(decl, "Amout", isConst, typeName);
+    plugin.visitVariableDeclaration(decl, "_amout", isConst, typeName);
+    plugin.visitVariableDeclaration(decl, "AMOUT", isConst, typeName);
+    plugin.visitVariableDeclaration(decl, "amountAvailablE", isConst, typeName);
+    plugin.visitVariableDeclaration(decl, "22amout", isConst, typeName);
+    plugin.visitVariableDeclaration(decl, "amoutVisible2", isConst, typeName);
+    plugin.visitVariableDeclaration(decl, "amout_visible", isConst, typeName);
 
-//     //correct method names
-//     plugin.visitFunctionDeclaration(decl, "process");
-//     plugin.visitFunctionDeclaration(decl, "processFile");
-//     plugin.visitFunctionDeclaration(decl, "calculateScore");
+    //correct method names
+    plugin.visitFunctionDeclaration(decl, "process");
+    plugin.visitFunctionDeclaration(decl, "processFile");
+    plugin.visitFunctionDeclaration(decl, "calculateScore");
 
-//     // //incorrect method names
-//     plugin.visitFunctionDeclaration(decl, "Process");
-//     plugin.visitFunctionDeclaration(decl, "_calculateScore");
-//     plugin.visitFunctionDeclaration(decl, "PROCESS_FILE");
-//     plugin.visitFunctionDeclaration(decl, "processFile_");
-//     plugin.visitFunctionDeclaration(decl, "processFile_2");
-//     plugin.visitFunctionDeclaration(decl, "process_file");
-// }
+    // //incorrect method names
+    plugin.visitFunctionDeclaration(decl, "Process");
+    plugin.visitFunctionDeclaration(decl, "_calculateScore");
+    plugin.visitFunctionDeclaration(decl, "PROCESS_FILE");
+    plugin.visitFunctionDeclaration(decl, "processFile_");
+    plugin.visitFunctionDeclaration(decl, "processFile_2");
+    plugin.visitFunctionDeclaration(decl, "process_file");
+}
 
-// TEST(PluginAPITests, AttributeNameTest)
-// {
-//     NamingConventionPlugin plugin;
-//     APIMock api;
-//     GenericTree decl = NULL;
-//     const bool isConst = false;
-//     const std::string typeName = "std::string";
-//     const AccessModifier access = AccessPublic;
-//     plugin.initialize(&api);
-//     EXPECT_CALL(api, warning(_,_))
-//     .Times(7);
-//     //correct names
-//     plugin.visitAttributeDeclaration(decl, access, "_amout", isConst, typeName);
-//     plugin.visitAttributeDeclaration(decl, access, "_i", isConst, typeName);
-//     plugin.visitAttributeDeclaration(decl, access, "_amoutOfChar", isConst, typeName);
-//     plugin.visitAttributeDeclaration(decl, access, "_isCircFigure", isConst, typeName);
+TEST(PluginAPITests, AttributeNameTest)
+{
+    NamingConventionPlugin plugin;
+    APIMock api;
+    GenericTree decl = NULL;
+    const bool isConst = false;
+    const std::string typeName = "std::string";
+    const AccessModifier access = AccessPublic;
+    plugin.initialize(&api);
+    EXPECT_CALL(api, warning(_,_))
+    .Times(7);
+    //correct names
+    plugin.visitAttributeDeclaration(decl, access, "_amout", isConst, typeName);
+    plugin.visitAttributeDeclaration(decl, access, "_i", isConst, typeName);
+    plugin.visitAttributeDeclaration(decl, access, "_amoutOfChar", isConst, typeName);
+    plugin.visitAttributeDeclaration(decl, access, "_isCircFigure", isConst, typeName);
 
-//     //incorrect names
-//     plugin.visitAttributeDeclaration(decl, access, "__isCircFigure", isConst, typeName);
-//     plugin.visitAttributeDeclaration(decl, access, "isCircFigure", isConst, typeName);
-//     plugin.visitAttributeDeclaration(decl, access, "_S", isConst, typeName);
-//     plugin.visitAttributeDeclaration(decl, access, "amout_available", isConst, typeName);
-//     plugin.visitAttributeDeclaration(decl, access, "Amout_available", isConst, typeName);
-//     plugin.visitAttributeDeclaration(decl, access, "_Amout_available", isConst, typeName);
-//     plugin.visitAttributeDeclaration(decl, access, "_amout_available", isConst, typeName);
-// }
+    //incorrect names
+    plugin.visitAttributeDeclaration(decl, access, "__isCircFigure", isConst, typeName);
+    plugin.visitAttributeDeclaration(decl, access, "isCircFigure", isConst, typeName);
+    plugin.visitAttributeDeclaration(decl, access, "_S", isConst, typeName);
+    plugin.visitAttributeDeclaration(decl, access, "amout_available", isConst, typeName);
+    plugin.visitAttributeDeclaration(decl, access, "Amout_available", isConst, typeName);
+    plugin.visitAttributeDeclaration(decl, access, "_Amout_available", isConst, typeName);
+    plugin.visitAttributeDeclaration(decl, access, "_amout_available", isConst, typeName);
+}
 
 // TEST(PluginAPITests, GlobalConstNameTest)
 // {
