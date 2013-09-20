bbd8bbe6b6cd15a8049ba8cbfdab8638f51d9ed0
Marcos Diaz <marcos.diaz@tallertechnologies.com>
2013-09-20 18:18:29 -0300

    Review

---
+++ namecheck/Visitor/NamingConventionCheckerInline.h
@@ -0,0 +1,75 @@
+/**
+* Taller Technologies - Software Development Company
+* Copyright 2013 - All rights reserved
+*
+* @file        NamingConventionCheckerInline.h
+* @author      Franco Riberi
+* @date        2013-09-06
+* @brief       Header file for namecheck providing NamingConventionChecker inline implementation.
+*/
+
+#ifndef NAMING_CONVENTION_CHECKER_INLINE_H
+#error Internal header file
+#endif
+
+#include "Visitor/NamingConventionChecker.h"
+
+inline void NamingConventionChecker::checkCorrectTypedefName(const std::string& declarationName, Result& result) const
+{
+    genericChecker(declarationName, _typedefRules, result);
+}
+
+inline void NamingConventionChecker::checkCorrectClassName(const std::string& declarationName, Result& result) const
+{
+    genericChecker(declarationName, _classRules, result);
+}
+
+inline void NamingConventionChecker::checkCorrectStructName(const std::string& declarationName, Result& result) const
+{
+    genericChecker(declarationName, _structRules, result);
+}
+
+inline void NamingConventionChecker::checkCorrectGlobalConstName(const std::string& declarationName, Result& result) const
+{
+    genericChecker(declarationName, _globalConstRules, result);
+}
+
+inline void NamingConventionChecker::checkCorrectEnumTypeName(const std::string& declarationName, Result& result) const
+{
+    genericChecker(declarationName, _enumTypeRules, result);
+}
+
+inline void NamingConventionChecker::checkCorrectEnumValueName(const std::string& declarationName, Result& result) const
+{
+    genericChecker(declarationName, _enumValueRules, result);
+}
+
+inline void NamingConventionChecker::checkCorrectMethodName(const std::string& declarationName, Result& result) const
+{
+    genericChecker(declarationName, _methodRules, result);
+}
+
+inline void NamingConventionChecker::checkCorrectVariableName(const std::string& declarationName, Result& result) const
+{
+    genericChecker(declarationName, _varRules, result);
+}
+
+inline void NamingConventionChecker::checkCorrectAttributeName(const std::string& declarationName, Result& result) const
+{
+    genericChecker(declarationName, _attributeRules, result);
+}
+
+inline void NamingConventionChecker::checkCorrectUnionName(const std::string& declarationName, Result& result) const
+{
+    genericChecker(declarationName, _unionRules, result);
+}
+
+inline void NamingConventionChecker::checkCorrectUnionValueName(const std::string& declarationName, Result& result) const
+{
+    genericChecker(declarationName, _unionValueRules, result);
+}
+
+inline void NamingConventionChecker::checkCorrectNamespaceName(const std::string& declarationName, Result& result) const
+{
+    genericChecker(declarationName, _namespaceRules, result);
+}
---
+++ tests/testFiles/c++/GlobalConstDeclarationTest.cpp
@@ -0,0 +1,19 @@
+const int CORRECT_GLOBAL(1);
+const int Incorrect_global(1);
+namespace Prueba
+{
+	const int incorrectns(1);
+	const int CORRECT_NS(2);
+};
+void test()
+{
+	const int correctfunc(2);
+	const int INCORRECTFUNC(2);
+}
+
+class Test
+{
+	const int _correctclass;
+	const int INCORRECT_CLASS;
+	Test() : _correctclass(1), INCORRECT_CLASS(1){};
+};
