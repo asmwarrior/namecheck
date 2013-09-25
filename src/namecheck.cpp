269e5be9dcc183716df95f05c563845390db4f9b
Marcos Diaz <marcos.diaz@tallertechnologies.com>
2013-09-25 14:13:31 -0300

    Fixed issue14(the plugin make gcc stop after without letting it finish with the process

--- src/namecheck.cpp
+++ src/namecheck.cpp
@@ -50,7 +50,6 @@ extern "C" void gate_callback_cpp_three(void*, void*)
         std::clog << "processing " << main_input_filename << std::endl;
         traverser.traverse(global_namespace, plugin->getVisitor());
     }
-    exit(EXIT_SUCCESS);
 }
 
 extern "C" void gate_callback_cpp_eleven(void*, void*)
@@ -67,15 +66,11 @@ extern "C" void gate_callback_cpp_eleven(void*, void*)
         std::clog << "processing with c++11 " << main_input_filename << std::endl;
         traverser.traverse(global_namespace, plugin->getVisitor());
     }
-    exit(EXIT_SUCCESS);
 }
 
 extern "C" int plugin_init(plugin_name_args* info, plugin_gcc_version* version)
 {
     std::cerr << "starting " << info->base_name << std::endl;
-
-    // Disable assembly output.
-    asm_file_name = HOST_BIT_BUCKET;
 
     if (!plugin_default_version_check(version, &gcc_version))
         return 1;
@@ -85,16 +80,13 @@ extern "C" int plugin_init(plugin_name_args* info, plugin_gcc_version* version)
         pathFile = info->argv->value;
     }
 
-    
-
-
     //implement this when trying to execute the plugin with c++0x or c++03
     // if(info->argc == 1 && (strcmp(info->argv->key,"c++0x") || strcmp(info->argv->key, "c++11")))
-    //     register_callback(info->base_name, PLUGIN_OVERRIDE_GATE, &gate_callback_cpp_eleven, 0);
+    //     register_callback(info->base_name, PLUGIN_EARLY_GIMPLE_PASSES_START, &gate_callback_cpp_eleven, 0);
     // else
-    //     register_callback(info->base_name, PLUGIN_OVERRIDE_GATE, &gate_callback_cpp_three, 0);
+    //     register_callback(info->base_name, PLUGIN_EARLY_GIMPLE_PASSES_START, &gate_callback_cpp_three, 0);
     
-    register_callback(info->base_name, PLUGIN_OVERRIDE_GATE, &gate_callback_cpp_three, 0);
+    register_callback(info->base_name, PLUGIN_EARLY_GIMPLE_PASSES_START, &gate_callback_cpp_three, 0);
     register_callback(info->base_name, PLUGIN_INFO, NULL, &namingInfo);
 
     return EXIT_SUCCESS;
