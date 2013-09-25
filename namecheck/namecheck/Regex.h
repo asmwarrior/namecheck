4a149603ce48459ef91ffb7d0a3538580ab374fa
Franco Riberi <franco.riberi@tallertechnologies.com>
2013-09-25 09:32:40 -0300

    Added traverser as independent project

---
+++ namecheck/namecheck/Regex.h
@@ -0,0 +1,45 @@
+/**
+* Taller Technologies - Software Development Company
+* Copyright 2013 - All rights reserved
+*
+* @file        Regex.h
+* @author      Franco Riberi
+* @date        2013-09-17
+* @brief       Header file for namecheck providing Regex class.
+*/
+
+#ifndef REGEX_H
+#define REGEX_H
+
+#include "Rule.h"
+
+namespace NamingChecker
+{
+
+/**
+ * @brief Specific Rule
+ *
+ * This class represents a regex which is a specific rule
+ */
+class Regex : public Rule
+{
+public:
+	Regex(const std::string& reg, const std::string& msg);	
+private:
+
+    virtual void checkRule(const std::string& declarationName, Result& result) const;
+
+    /**
+     * @brief Represent a specific regex
+     */
+    BoostRegex _regex;
+
+    /**
+     * @brief Represent a error message
+     */
+    Message _errmsg;
+};
+
+} //end namespace
+
+#endif
---
+++ namecheck/src/Regex.cpp
@@ -0,0 +1,29 @@
+/**
+* Taller Technologies - Software Development Company
+* Copyright 2013 - All rights reserved
+*
+* @file        Regex.cpp
+* @author      Franco Riberi
+* @date        2013-09-17
+* @brief       This is the implementation of Regex interface
+*/
+
+#include <iostream>
+#include "namecheck/Regex.h"
+
+namespace NamingChecker
+{
+
+Regex::Regex(const std::string& reg, const std::string& msg): _regex(reg), _errmsg(msg)
+{}
+
+void Regex::checkRule(const std::string& declarationName, Result& result) const
+{
+    result._match = boost::regex_match(declarationName, _regex);
+    if (!result._match)
+        result._message = _errmsg;
+    else
+        result._message.clear();
+}
+
+} // end namespace
