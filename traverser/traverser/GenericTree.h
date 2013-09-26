7066343594771cceafbf2091bfc29f174e994605
Franco Riberi <franco.riberi@tallertechnologies.com>
2013-09-26 11:27:35 -0300

    Added header comments with GPL license (traverser project)

--- traverser/traverser/GenericTree.h
+++ traverser/traverser/GenericTree.h
@@ -1,12 +1,36 @@
 /**
-* Taller Technologies - Software Development Company
-* Copyright 2013 - All rights reserved
-*
-* @file        GenericTree.h
-* @author      Fracisco Herrero and Marcos Diaz
-* @date        2013-09-06
-* @brief       Header file for namecheck providing GenericTree type.
-*/
+ * @file     GenericTree.h
+ * @brief    This file contains all the definitions needed in traverser
+ *
+ * @author   Francisco Herrero
+ * @email    francisco.herrero AT tallertechnologies.com
+ *
+ * @author   Marcos Diaz
+ * @email    marcos.diaz AT tallertechnologies.com
+ *
+ * Contents: Header file for traverser providing GenericTree type.
+ *
+ * System:    traverser
+ * Language:  C++
+ *
+ * @date      September 13, 2013
+ *
+ * This file is part of traverser.
+ *
+ * traverser is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * traverser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with traverser. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #ifndef GCC_GENERIC_TREE_H
 #define GCC_GENERIC_TREE_H
@@ -14,29 +38,29 @@
 #include <gmp.h>
 
 #if (__GNUC__ == 4) && (__GNUC_MINOR__ == 6)
-	extern "C"
-	{
-	#include "config.h"
-	#undef HAVE_DECL_GETOPT
-	#define HAVE_DECL_GETOPT 1
-	#include "gcc-plugin.h"
-	#include "tree.h"
-	}
+extern "C"
+{
+#include "config.h"
+#undef HAVE_DECL_GETOPT
+#define HAVE_DECL_GETOPT 1
+#include "gcc-plugin.h"
+#include "tree.h"
+}
 #else
-	#include "config.h"
-	#undef HAVE_DECL_GETOPT
-	#define HAVE_DECL_GETOPT 1
-	#include "gcc-plugin.h"
-	#include "tree.h"
+#include "config.h"
+#undef HAVE_DECL_GETOPT
+#define HAVE_DECL_GETOPT 1
+#include "gcc-plugin.h"
+#include "tree.h"
 #endif
 
 
 namespace GPPGeneric
 {
 
-/** 
+/**
 * @brief A tree node (is a pointer type).
-* 
+*
 */
 typedef tree GenericTree;
 
