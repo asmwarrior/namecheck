9da9980c887d1b7b2c3ee4870806df81a9b15bc0
franco riberi <franco.riberi@tallertechnologies.com>
2013-10-08 10:44:19 -0300

    Updated default regex (accepted digits in the statements) and test

--- namecheck/src/LowerCamelCaseRule.cpp
+++ namecheck/src/LowerCamelCaseRule.cpp
@@ -39,11 +39,11 @@ LowerCamelCaseRule::LowerCamelCaseRule() : RegexCollection(3)
 {
     _regexs[0] = "^\\l.*?";
     _regexs[1] = "^_?[\\l\\u\\d]*$";
-    _regexs[2] = ".*?[^\\u_\\d]\\l?$";
+    _regexs[2] = ".*?[\\l\\d]$";
 
     _errmsgs[0] = gettext("should start with lowercase");
     _errmsgs[1] = gettext("should not be written using underscore, only is supported one underscore at the beginning");
-    _errmsgs[2] = gettext("should end with lowercase");
+    _errmsgs[2] = gettext("should end with lowercase or digit");
 }
 
 } //end namespace
